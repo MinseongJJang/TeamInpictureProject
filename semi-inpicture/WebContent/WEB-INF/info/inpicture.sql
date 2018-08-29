a8df68c9c63154c9e2c6dfba1233f41f03b9ac53
Jieun <jiuen0113@gmail.com>
2018-08-29 16:14:34 +0900

    ddd

--- semi-inpicture/WebContent/WEB-INF/info/inpicture.sql
+++ semi-inpicture/WebContent/WEB-INF/info/inpicture.sql
@@ -103,6 +103,7 @@ create table auction(
 	auction_state number default 0,
 	auction_main_pic varchar2(100) not null,
 	auction_begin_price number not null,
+	auction_final_bidder varchar2(100) not null,
 	constraint fk_auction_auction_no foreign key(auction_no) references auction_apply(auction_no) on delete cascade
 )
 
