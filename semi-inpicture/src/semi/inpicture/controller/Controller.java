9fdf6a950c0c285e8299317047505d53b348dc05
user <user@192.168.0.160>
2018-08-30 18:20:27 +0900

    Exception 수정

--- semi-inpicture/src/semi/inpicture/controller/Controller.java
+++ semi-inpicture/src/semi/inpicture/controller/Controller.java
@@ -1,12 +1,9 @@
 package semi.inpicture.controller;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Controller {
 	public String execute(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException;
+			throws Exception;
 }
