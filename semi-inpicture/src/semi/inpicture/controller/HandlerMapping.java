5f44ba6f901f6147594bf8c8c410658cbed3c074
user <user@192.168.0.160>
2018-08-30 18:21:46 +0900

    Login,Logout,Login 컨트롤러 추가

--- semi-inpicture/src/semi/inpicture/controller/HandlerMapping.java
+++ semi-inpicture/src/semi/inpicture/controller/HandlerMapping.java
@@ -15,7 +15,13 @@ public class HandlerMapping {
 	
 	public Controller create(String command) {
 		Controller controller = null;
-		
+		if(command.equals("Login")) {
+			controller=new LoginController();
+		}else if(command.equals("Logout")){
+			controller=new LogoutController();
+		}else if(command.equals("LoginForm")){
+			controller=new LoginFormController();
+		}
 		return controller;
 	}
 }
